c06109bf8c80408122887dd7680dfdd951e2b9d4
wordtourney <jakegrantche@gmail.com>
2015-05-19 15:01:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var main = function() {
+  $('.oh-seven').click(function() {
+    $('.tourney-oh-seven').toggleClass('hidden');
+  });
+  
+  $('.oh-eight').click(function() {
+    $('.tourney-oh-eight').toggleClass('hidden');
+  });
+
+}
 
 $(document).ready(main);

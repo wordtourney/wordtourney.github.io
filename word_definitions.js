bc98a67886a0371a98b25388f29211ace9aa2fb0
wordtourney <jakegrantche@gmail.com>
2016-01-15 14:32:24 -0600

    Update word_definitions.js

--- word_definitions.js
+++ word_definitions.js
@@ -2,10 +2,52 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.definition = [
     {
-      def_word: '',
-      def_partofspeech: '',
-      def_definition: '',
-      def_class: ''
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
+    },
+    {
+      def_word: 'test',
+      def_partofspeech: 'v',
+      def_definition: 'test is a test test test',
+      def_class: 'test'
     }
   ]
 }]);
